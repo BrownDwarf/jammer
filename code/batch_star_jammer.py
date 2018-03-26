94a70f7419526a2c926c7592db7bb543c4f18b3b
gully <igully@gmail.com>
2018-03-26 15:55:50 -0700

    ignore the Blackbody function, apparently the astropy API changed.

--- code/batch_star_jammer.py
+++ code/batch_star_jammer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import os
 
-chs = list(range(5, 30))
+chs = list(range(5, 6))
 
 os.chdir(os.path.expandvars('$jammer/sf/2M0314/'))
 
 for ch in chs:
-    print(m)
+    print(ch)
     os.system("cp K_ch004/user_prior.py K_ch{:03d}/".format(ch))
     os.chdir("K_ch{:03d}/output/marley_grid/run01/".format(ch))
-    os.system('time $jammer/code/star_marley_beta.py --samples=5000 --incremental_save=100 > run.out')
+    os.system('time $jammer/code/star_marley_beta.py --samples=5 --incremental_save=1 > run.out')
     os.chdir(os.path.expandvars('$jammer/sf/2M0314/'))
